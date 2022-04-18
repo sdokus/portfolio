3371081a69638dc74c3e41ed58abb4de229ba9de
sdokus <sami2464@gmail.com>
2022-04-18 16:46:28 -0400

    added: intro component

---
+++ src/components/intro/intro.jsx
@@ -0,0 +1,33 @@
+import "./intro.css"
+import Me from "../../img/me2.jpg"
+
+ const Intro = () => {
+  return (
+    <div className="i">
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className = "i-intro">Hello, My name is</h2>
+          <h1 className="i-name">Sam Dokus</h1>
+          <div className="i-title">
+          <div className="i-title-wrapper">
+          <div className="i-title-item"> Fullstack Developer</div>
+          <div className="i-title-item"> Web Developer</div>
+          <div className="i-title-item"> Problem Solver</div>
+          <div className="i-title-item"> Designer/Painter</div>
+          <div className="i-title-item"> Friendly Human</div>
+            </div>
+          </div>
+          <p className="i-desc">
+          I love the structure and logic that data and coding provide and look forward to utilizing my organizational skills to creatively solve problems.
+          </p>
+        </div>
+      </div>
+      <div className="i-right">
+        <div className="i-bg"></div>
+        <img src={Me} alt="" className="i-img" />
+      </div>
+    </div>
+  )
+}
+
+export default Intro
