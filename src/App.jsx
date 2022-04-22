bcbf3cd45d54f779d85ff88221c11eb70d838848
sdokus <sami2464@gmail.com>
2022-04-22 14:03:37 -0400

    added: toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,27 @@ import Intro from './components/intro/Intro';
 import About from './components/about/About'
 import ProjectList from './components/projectList/ProjectList';
 import Contact from './components/contact/Contact'
+import Toggle from './components/toggle/Toggle'
+import {ThemeContext} from './context'
+import { useContext } from 'react';
 
 const App = () => {
-  return <div>
+  const theme = useContext(ThemeContext)
+  const darkMode = theme.state.darkMode;
+  return (
+    <div
+    style={{
+      backgroundColor: darkMode ? "#222" : "white",
+      color: darkMode && "white",
+    }}
+  >
+    <Toggle />
     <Intro />
     <About />
     <ProjectList />
     <Contact />
-  </div>;
+  </div>
+  );
 };
 
 export default App;
